54ca065ff5944703ec23f3ba66d2c0472470b11f
vynas <hamdabousnina@gmail.com>
2025-04-28 08:39:59 -0400

    Authentication: setting up session, Getting the current user signed in

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,44 +1,48 @@
-import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
-import { UsersService } from "./users.service";
-import { randomBytes, scrypt as _scrypt } from "crypto";
-import { promisify } from "util";
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
-const scrypt = promisify(_scrypt)
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService) {}
 
-    async signup(email: string, password: string) {
-        // check if email exists
-        const users = await this.usersService.find(email);
-        if (users.length) {
-            throw new BadRequestException("Email in use");
-        }
-        // hash password
-        //generate a salt
-        const salt = randomBytes(8).toString("hex")
-        //hash the salt and password together
-        const hash = await scrypt(password, salt, 32) as Buffer
-        // join the hashed result and salt together
-        const result = salt + "." + hash.toString("hex")
-
-        // create a new user and save it to the database
-        const user = await this.usersService.create(email, result);
-        // return user
-        return user
+  async signup(email: string, password: string) {
+    // check if email exists
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email in use');
     }
+    // hash password
+    //generate a salt
+    const salt = randomBytes(8).toString('hex');
+    //hash the salt and password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    // join the hashed result and salt together
+    const result = salt + '.' + hash.toString('hex');
 
-    async signin(email: string, password: string) {
-        const [user] = await this.usersService.find(email);
-        if (!user) {
-            throw new NotFoundException("User not found");
-        }
-        const [salt, hashedPassword] = user.password.split(".");
-        const hash = (await scrypt(password, salt, 32)) as Buffer;
-        if (hashedPassword !== hash.toString("hex")) {
-            throw new BadRequestException("Invalid password");
-        } 
-            return user;
+    // create a new user and save it to the database
+    const user = await this.usersService.create(email, result);
+    // return user
+    return user;
+  }
+
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+    const [salt, hashedPassword] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    if (hashedPassword !== hash.toString('hex')) {
+      throw new BadRequestException('Invalid password');
+    }
+    return user;
+  }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
   UseInterceptors,
   ClassSerializerInterceptor,
+  Session,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
@@ -20,17 +21,29 @@ import { AuthService } from './auth.service';
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService, private authService: AuthService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  @Get('/current')
+  WhoAmI(@Session() session: any) {
+    return this.usersService.findOne(session.userId);
+  }
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password)
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Post('/signin')
-    signin(@Body() body: CreateUserDto) {
-      return this.authService.signin(body.email, body.password)
-    }
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
 
   @Get('/:id')
   findUser(@Param('id') id: string) {
