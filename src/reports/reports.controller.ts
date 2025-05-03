9987d562be271ad597b349cc4f903398d9bb642a
vynas <hamdabousnina@gmail.com>
2025-05-03 08:25:43 -0400

    add admin guard, assign currentUser with a middleware, validate Query string value, Transform Query string value

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,6 +5,8 @@ import {
   Param,
   Post,
   UseGuards,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { CreateReportDto } from './dto/create-report.dto';
 import { ReportsService } from './reports.service';
@@ -14,6 +16,8 @@ import { UserEntity } from '../users/entities/user.entity';
 import { ReportDto } from './dto/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dto/approve-report.dto';
+import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dto/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -25,7 +29,11 @@ export class ReportsController {
     return this.reportsService.create(body, user);
   }
 
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {}
+
   @Patch('/:id')
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
